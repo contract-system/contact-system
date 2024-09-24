1f68c1dd622ed70cebd689b243dd4f2684fbdaf7
Noor-Aldin1 <nooraldin.aboalsaid@gmail.com>
2024-09-24 20:47:57 +0300

    I have created a routes for the user and for the admin as well.

---
+++ backend/routes/user.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\ContractController;
+
+
+
+
+
+// Just for testing 
+Route::prefix('contracts')->group(function () {
+    Route::get('/', [ContractController::class, 'getAllContract']); // Fetch all contracts
+    Route::get('/{contractId}', [ContractController::class, 'getOneContract']); // Fetch a single contract by ID
+    Route::post('/', [ContractController::class, 'storeContract']); // Create a new contract
+    Route::put('/{contractId}', [ContractController::class, 'updateContract']); // Update a contract
+    Route::delete('/{contractId}', [ContractController::class, 'deleteContract']); // Delete a contract 
+});

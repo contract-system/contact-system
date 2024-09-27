31133a4e9b31eb17db0fcdc38f7b893675c30dc8
Omarhudaib <168991559+Omarhudaib@users.noreply.github.com>
2024-09-27 20:21:56 +0300

    omar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 // src/App.js
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header_pricing';
 import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
-import Header from "./components/Header/Header_pricing";
-import Footer from "./components/Footer/Footer";
-import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
@@ -20,15 +16,7 @@ const App = () => {
     <>
       <Header />
       <Hero />
-      {/* Uncomment this if you want to show the Hero section */}
-      {/* <Hero /> */}
-      {/* Uncomment this if you want to show the Services section */}
-      {/* <Services /> */}
-      {/* Uncomment this if you want to show the About section */}
-      {/* <About /> */}
-      {/* Uncomment this if you want to show the Contact section */}
-      {/* <Contact /> */}
-      <Profile />
+
       <Footer />
     </>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,25 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './styles/main.css'; // Your custom styles
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/contract/layout'; // Import your Layout component
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-
-import Layout from "./components/contract/layout"; // Import your Layout component
 import Profile from "./components/Profile/Profile";
 import Subscription from "./components/Profile/Subscription";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./styles/main.css"; // Your custom styles
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
